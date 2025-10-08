4a18c10dd81ae07fb966f37e6e9a26f458f77874
Jsabs <sabillano057@gmail.com>
2025-10-08 14:58:34 +0800

    Fruits You tapped

--- lib/main.dart
+++ lib/main.dart
@@ -5,51 +5,34 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final List<String> fruits = ['Jev', 'Jay', 'Leonard', 'Raymundo', 'Francis', 'Jumlord', 'Jomari', 'Jomel', 'Jomike', 'Jomarie'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Static Flutter App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override 
-  Widget build(BuildContext context){
-    return Scaffold(
-      body:Column(
-        children: [
-          Expanded(
-            child: Container(
-              color: Colors.black,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.green,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.yellow,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.red,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.black,
-            ),
-          ),
-        ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Friend Names'),
+        ),
+        body: ListView.builder(
+          itemCount: fruits.length,
+          itemBuilder: (context, index) {
+            final fruit = fruits[index];
+            return Card(
+              margin: const EdgeInsets.symmetric(vertical:8, horizontal: 16),
+              child: ListTile(
+                leading: const Icon(Icons.local_grocery_store),
+                title: Text(fruit),
+                subtitle: Text('Item Number: ${index + 1}'),
+                onTap: (){
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('You tapped $fruit!'))
+                  );
+                },
+              ),
+            );
+          },
+        ),
       ),
     );
   }
--- lib/tuts.dart
+++ lib/tuts.dart
@@ -104,50 +104,118 @@ Scaffold(
 
 
 
-September 10 2025
+// September 10 2025
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-      ),
-      body: Container(
-        padding: EdgeInsets.all(24.0),
-        margin: EdgeInsets.fromLTRB(30.0, 20.0, 23.0, 20.0),
-        color: const Color.fromARGB(255, 201, 188, 188),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
-              children:[
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Text('This is My Heading Column Flutter Activity'),
-                ),
-              ],
-            ),
-            Row(
-              children: const[
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Icon(Icons.home, size:40, color: Colors.blue,),
-                ),
-                Text('Hello World'),
-                Icon(Icons.favorite, size:40, color: Colors.red,),
-                Text('Everything People'),
-                Icon(Icons.warning, size:40, color: Colors.amber,),
-              ],
-            ),
-            Column(
-              children:[Text('Flutter Run'), Text('Flutter Doctor')],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context){
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Home Page'),
+//         centerTitle: true,
+//         backgroundColor: Colors.blue,
+//       ),
+//       body: Container(
+//         padding: EdgeInsets.all(24.0),
+//         margin: EdgeInsets.fromLTRB(30.0, 20.0, 23.0, 20.0),
+//         color: const Color.fromARGB(255, 201, 188, 188),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Column(
+//               children:[
+//                 Padding(
+//                   padding: EdgeInsets.all(10),
+//                   child: Text('This is My Heading Column Flutter Activity'),
+//                 ),
+//               ],
+//             ),
+//             Row(
+//               children: const[
+//                 Padding(
+//                   padding: EdgeInsets.all(10),
+//                   child: Icon(Icons.home, size:40, color: Colors.blue,),
+//                 ),
+//                 Text('Hello World'),
+//                 Icon(Icons.favorite, size:40, color: Colors.red,),
+//                 Text('Everything People'),
+//                 Icon(Icons.warning, size:40, color: Colors.amber,),
+//               ],
+//             ),
+//             Column(
+//               children:[Text('Flutter Run'), Text('Flutter Doctor')],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+// PAST ACTIVITY
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Static Flutter App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+// class HomePage extends StatelessWidget {
+//   @override 
+//   Widget build(BuildContext context){
+//     return Scaffold(
+//       body:Column(
+//         children: [
+//           Expanded(
+//             child: Container(
+//               color: Colors.black,
+//             ),
+//           ),
+//           Expanded(
+//             child: Container(
+//               color: Colors.green,
+//             ),
+//           ),
+//           Expanded(
+//             child: Container(
+//               color: Colors.yellow,
+//             ),
+//           ),
+//           Expanded(
+//             child: Container(
+//               color: Colors.red,
+//             ),
+//           ),
+//           Expanded(
+//             child: Container(
+//               color: Colors.black,
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
